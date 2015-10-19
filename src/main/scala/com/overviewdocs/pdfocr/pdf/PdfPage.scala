f2354d9d5905ecea085f11f8189ff16c5b69a0aa
Adam Hooper <adam@adamhooper.com>
2015-10-19 15:58:20 -0400

    Add PdfPage.toImage

--- src/main/scala/com/overviewdocs/pdfocr/pdf/PdfPage.scala
+++ src/main/scala/com/overviewdocs/pdfocr/pdf/PdfPage.scala
@@ -14,13 +14,51 @@ import com.overviewdocs.pdfocr.exceptions.PdfInvalidException
   * A PdfPage will only be valid so long as its parent PdfDocument's `close`
   * method has not been called.
   */
-class PdfPage(pdfDocument: PdfDocument, pdPage: PDPage, pageNumber: Int) {
+class PdfPage(val pdfDocument: PdfDocument, val pdPage: PDPage, val pageNumber: Int) {
+  private val PdfDpi: Int = 72 // always. Part of the PDF spec.
+  private val ImageDpi: Int = 300
+  private val MaxResolution: Int = 4000
   //def toImage: BufferedImage
   //def toPdf: Array[Byte]
+
+  /** Returns all the text we can read from the document. */
   def toText: String = {
     val stripper = new PDFTextStripper
     stripper.setStartPage(pageNumber + 1)
     stripper.setEndPage(pageNumber + 1)
     stripper.getText(pdfDocument.pdDocument)
+  }
+
+  /** Returns how many dots-per-inch we should render an image.
+    *
+    * The result will be 300, unless the PDF is large. If the PDF is large, the
+    * DPI will max out at the largest integer that makes the output image
+    * smaller than 4000x4000.
+    *
+    * If the page is missing a media box, the DPI will be 1.
+    */
+  private def bestDpi: Int = {
+    var dpi = ImageDpi
+
+    Option(pdPage.getMediaBox) match {
+      case Some(rect) => {
+        var dpi = ImageDpi
+
+        if (rect.getWidth * dpi / PdfDpi > MaxResolution) {
+          dpi = MaxResolution * PdfDpi / rect.getWidth.toInt
+        }
+        if (rect.getHeight * dpi / PdfDpi > MaxResolution) {
+          dpi = MaxResolution * PdfDpi / rect.getHeight.toInt
+        }
+        
+        dpi
+      }
+      case None => 1
+    }
+  }
+
+  /** Renders the page to an image. */
+  def toImage: BufferedImage = {
+    pdPage.convertToImage(BufferedImage.TYPE_BYTE_GRAY, bestDpi)
   }
 }
--- src/test/scala/com/overviewdocs/pdfocr/pdf/PdfPageSpec.scala
+++ src/test/scala/com/overviewdocs/pdfocr/pdf/PdfPageSpec.scala
@@ -2,6 +2,7 @@ package com.overviewdocs.pdfocr.pdf
 
 import java.nio.file.Paths
 import org.apache.pdfbox.pdmodel.{PDDocument,PDPage}
+import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream
 import org.apache.pdfbox.pdmodel.font.PDType1Font
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -11,6 +12,7 @@ import com.overviewdocs.pdfocr.exceptions.PdfInvalidException
 import com.overviewdocs.pdfocr.test.UnitSpec
 
 class PdfPageSpec extends UnitSpec {
+  /** Loads a PDF document from the "trivial-pdfs" folder. */
   private def load(resourceName: String): Future[PdfDocument] = {
     val pathString: String = try {
       getClass.getResource(s"/trivial-pdfs/$resourceName").toString.replace("file:", "")
@@ -23,37 +25,104 @@ class PdfPageSpec extends UnitSpec {
     PdfDocument.load(path)
   }
 
+  /** Creates a "Hello, world!" document. */
+  private def helloWorld: (PdfDocument,PdfPage) = {
+    val pdDocument = new PDDocument()
+    val pdfDocument = new PdfDocument(Paths.get(""), pdDocument)
+    val pdPage = new PDPage()
+    pdDocument.addPage(pdPage)
+    val stream = new PDPageContentStream(pdDocument, pdPage)
+    stream.beginText
+    stream.setFont(PDType1Font.HELVETICA, 18)
+    stream.moveTextPositionByAmount(1, 1)
+    stream.drawString("Hello, world!")
+    stream.endText
+    stream.close
+    (pdfDocument, new PdfPage(pdfDocument, pdPage, 0))
+  }
+
+  /** Creates a page that has an invalid stream. */
+  private def loadInvalidStreamPage: (PdfDocument,PdfPage) = {
+    val pdf = load("2nd-page-invalid.pdf").futureValue
+
+    val pageIt = pdf.pages
+    pageIt.next.futureValue // skip page 1
+    val page = pageIt.next.futureValue
+
+    (pdf, page)
+  }
+
   describe("toText") {
     it("should return text") {
-      val pdDocument = new PDDocument()
-      val pdfDocument = new PdfDocument(Paths.get(""), pdDocument)
-      val pdPage = new PDPage()
-      pdDocument.addPage(pdPage)
-      val stream = new PDPageContentStream(pdDocument, pdPage)
-      stream.beginText
-      stream.setFont(PDType1Font.HELVETICA, 18)
-      stream.moveTextPositionByAmount(1, 1)
-      stream.drawString("Hello, world!")
-      stream.endText
-      stream.close
-
+      val (document, page) = helloWorld
       try {
-        val page = new PdfPage(pdfDocument, pdPage, 0)
         page.toText must equal("Hello, world!\n")
       } finally {
-        pdDocument.close
+        document.close
       }
     }
 
     it("should return an empty string when a content stream is invalid") {
-      val pdf = load("2nd-page-invalid.pdf").futureValue
+      val (document, page) = loadInvalidStreamPage
 
-      val pageIt = pdf.pages
-      pageIt.next.futureValue // skip page 1
-      val page = pageIt.next.futureValue
+      try {
+        page.toText must equal("\n")
+      } finally {
+        document.close
+      }
+    }
+  }
 
-      page.toText must equal("\n")
-      pdf.close
+  describe("toImage") {
+    it("should return an image at 300dpi") {
+      val (document, page) = helloWorld
+      page.pdPage.setMediaBox(new PDRectangle(72, 144))
+
+      try {
+        val image = page.toImage
+        image.getWidth must equal(300)
+        image.getHeight must equal(600)
+      } finally {
+        document.close
+      }
+    }
+
+    it("should max out at 4000px horizontally") {
+      val (document, page) = helloWorld
+      page.pdPage.setMediaBox(new PDRectangle(7200, 144))
+
+      try {
+        val image = page.toImage
+        image.getWidth must equal(4000)
+        image.getHeight must equal(80) // 4000 * 144 / 7200
+      } finally {
+        document.close
+      }
+    }
+
+    it("should max out at 4000px vertically") {
+      val (document, page) = helloWorld
+      page.pdPage.setMediaBox(new PDRectangle(144, 7200))
+
+      try {
+        val image = page.toImage
+        image.getWidth must equal(80) // 4000 * 144 / 7200
+        image.getHeight must equal(4000)
+      } finally {
+        document.close
+      }
+    }
+
+    it("should return a blank image when a content stream is invalid") {
+      val (document, page) = loadInvalidStreamPage
+
+      try {
+        val image = page.toImage
+        image.getWidth must equal(1)
+        image.getHeight must equal(1)
+      } finally {
+        document.close
+      }
     }
   }
 }
