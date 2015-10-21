fe3582b7be94dbc62d50ba74086faf9dc8e84512
Adam Hooper <adam@adamhooper.com>
2015-10-21 14:02:41 -0400

    Increase test timeout

--- src/test/scala/org/overviewproject/pdfocr/test/UnitSpec.scala
+++ src/test/scala/org/overviewproject/pdfocr/test/UnitSpec.scala
@@ -2,5 +2,8 @@ package org.overviewproject.pdfocr.test
 
 import org.scalatest.{BeforeAndAfter,FunSpec,MustMatchers}
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Span,Millis}
 
-abstract class UnitSpec extends FunSpec with MustMatchers with ScalaFutures
+abstract class UnitSpec extends FunSpec with MustMatchers with ScalaFutures {
+  implicit override def patienceConfig = PatienceConfig(timeout = Span(2000, Millis), interval = Span(15, Millis))
+}
